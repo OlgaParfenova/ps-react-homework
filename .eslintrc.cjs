66e8c9c95485476370c702d47b8fd6e9444b8fc7
Olga Parfenova <work.parfenova@gmail.com>
2023-11-07 13:55:54 +0300

    ES lint file has been updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    semi: ['error', 'always', { omitLastInOneLineBlock: false }],
+    'comma-dangle': ['error', 'only-multiline'],
+    quotes: ['error', 'single'],
+    'react/prop-types': [0],
   },
-}
+};
